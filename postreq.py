279187ab09ed03065c90110a03936f97c0466154
Siroun Petersen <58755585+sirounpetersen@users.noreply.github.com>
2021-06-30 20:02:53 +0000

    changed api source requests

--- postreq.py
+++ postreq.py
@@ -1,22 +1,40 @@
 import requests
-from keys, import keys
-from tweepy import API, OAuthHandler
+import json
+
+spellsurl = "https://www.dnd5eapi.co/api/spells"
+r = requests.get(spellsurl)
+
+spellsdict = (r.json())
+
+#maze
+a = (spellsdict["results"][198]["index"])
+mazeurl = spellsurl +"/" + a 
+
+q = requests.get(mazeurl)
+mazedict = q.json()
 
 
 
-auth = OAuthHandler(apikey, apisecret)
-api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-trends = api.trends_place(id=23424977)
-trending = []
-for trend in trends[0]["trends"]:
-    trending.append(trend["name"])
-print(trending)
-
-# url = "https://api.twitter.com/1.1/statuses/update.json"
-# obj = {"status": "value"}
-
-# response = requests.post(url, data = obj)
-# print(response.json())
-
+"""{'index': 'maze', 
+'name': 'Maze', 
+'desc': ['You banish a creature that you can see within range into a labyrinthine demiplane. The target remains there for the duration or until it escapes the maze.', 
+  'The target can use its action to attempt to escape. When it does so, it makes a DC 20 Intelligence check. If it succeeds, it escapes, and the spell ends (a minotaur or goristro demon automatically succeeds).', 
+  'When the spell ends, the target reappears in the space it left or, if that space is occupied, 
+  in the nearest unoccupied space.'], 
+'range': '60 feet', 
+'components': ['V', 'S'], 
+'ritual': False, 
+'duration': 'Up to 10 minutes', 
+'concentration': True, 
+'casting_time': '1 action', 
+'level': 8, 
+'school': {'index': 'conjuration', 
+'name': 'Conjuration', 
+'url': '/api/magic-schools/conjuration'}, 
+'classes': 
+  [{'index': 'wizard', 'name': 'Wizard', 'url': '/api/classes/wizard'}], 
+'subclasses': [], 
+'url': '/api/spells/maze'}
+"""
